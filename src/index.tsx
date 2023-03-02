ec7b3bca835fa882eaf80a37beb0d8bb72f7afdc
Chiaki.C <chiaki@portto.com>
2023-03-02 17:02:22 +0800

    [Feature] Update blocto wallet to v2

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,36 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
-import * as fcl from "@onflow/fcl";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import theme from "./theme";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ChakraProvider } from '@chakra-ui/react'
+import * as fcl from '@onflow/fcl'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import theme from './theme'
 
-const isMainnet = process.env.REACT_APP_NETWORK === "mainnet";
-const NODE_URL = isMainnet
-  ? "https://access-mainnet-beta.onflow.org"
-  : "https://access-testnet.onflow.org";
+const isMainnet = process.env.REACT_APP_NETWORK === 'mainnet'
+const NODE_URL = isMainnet ? 'https://rest-mainnet.onflow.org' : 'https://rest-testnet.onflow.org'
 const WALLET_URL = isMainnet
-  ? "https://flow-wallet.blocto.app/api/flow/authn"
-  : "https://flow-wallet-testnet.blocto.app/api/flow/authn";
+  ? 'https://wallet-v2.blocto.app/api/flow/authn'
+  : 'https://wallet-v2-dev.blocto.app/api/flow/authn'
 
 fcl
   .config()
-  .put("challenge.scope", "email") // request for Email
-  .put("accessNode.api", NODE_URL)
-  .put("discovery.wallet", WALLET_URL) // Blocto testnet wallet
-  .put("discovery.wallet.method", "HTTP/POST")
-  .put("service.OpenID.scopes", "email!");
+  .put('challenge.scope', 'email') // request for Email
+  .put('accessNode.api', NODE_URL)
+  .put('discovery.wallet', WALLET_URL) // Blocto testnet wallet
+  .put('discovery.wallet.method', 'HTTP/POST')
+  .put('service.OpenID.scopes', 'email!')
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
